c149ae77c2a628a7732c2b91c43d1f4937cbb908
Ankur Sonaniya <ankur@Ankurs-MacBook-Air.local>
2025-11-22 12:55:45 +0530

    real insta

--- script.js
+++ script.js
@@ -1,49 +1,28 @@
-var elem =document.querySelectorAll(".elem")
+var arr = [{
+    dp:"https://images.unsplash.com/photo-1745523669653-25ac9bf21bbc?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1020&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},{
+    dp:"https://plus.unsplash.com/premium_photo-1695575576052-7c271876b075?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZXxlbnwwfDF8MHx8fDA%3D",story:"https://images.unsplash.com/photo-1545291730-faff8ca1d4b0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fG1vZGV8ZW58MHwxfDB8fHww"
+}, {
+    dp:"https://images.unsplash.com/photo-1566206091558-7f218b696731?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fG1vZGV8ZW58MHwxfDB8fHww",story:"https://images.unsplash.com/photo-1589363358751-ab05797e5629?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fG1vZGV8ZW58MHwxfDB8fHww"
+},{
+    dp:"https://images.unsplash.com/photo-1535530705774-695729778c55?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fG1vZGV8ZW58MHwxfDB8fHww",story:"https://plus.unsplash.com/premium_photo-1675186049302-a0dad4cf3412?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+}]
 
-elem.forEach(function(val){
-    val.addEventListener("mouseenter",function(){
-        val.childNodes[3].style.opacity = 1
-    })
-    val.addEventListener("mouseleave",function(){
-        val.childNodes[3].style.opacity = 0
-    })
-    val.addEventListener("mousemove",function(dets){
-        val.childNodes[3].style.left = dets.x+"px"
-        val.childNodes[3].style.top = dets.y+"px"
-
-    })
+var cluter = ""
+arr.forEach(function(elem,idx){
+    cluter += `<div class="story">
+                <img id = "${idx}" src="${elem.dp}" alt="">
+            </div>`
 })
 
+document.querySelector("#stories").innerHTML = cluter;
 
+document.querySelector("#stories").addEventListener("click",function(dets){
+    document.querySelector("#fullscreen").style.display = "block"
+    document.querySelector("#fullscreen").style.backgroundImage = `url(${arr[dets.target.id].story})`
 
+    setTimeout(function(){
+    document.querySelector("#fullscreen").style.display = "none"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var img1 = document.querySelector("#elem1 img")
-
-// elem1.addEventListener("mousemove", function(dets){
-//     img1.style.left = dets.x+"px"
-//     img1.style.top = dets.y+"px"
-// })
-
-// elem1.addEventListener("mouseenter", function(dets){
-//     img1.style.opacity = 1
-// })
-
-// elem1.addEventListener("mouseleave", function(dets){
-//     img1.style.opacity = 0
-// })
+    },3000)
+});

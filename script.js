d16652020f2184e84e25d6ef85ae0d06365b0eaa
Ankur Sonaniya <ankur@Ankurs-MacBook-Air.local>
2025-11-22 12:43:53 +0530

    insta story

--- script.js
+++ script.js
@@ -1,70 +1,13 @@
-var arr = [
-    { 
-        team : "csk" ,
-        primary: "yellow",
-        secondary: "blue",
-        captain: "rohit sharma",
-        win: "6 times"
-    },{
-        team : "kkr" ,
-        primary: "purple",
-        secondary: "goldenrod",
-        captain: "subman gill",
-        win: "1 times"
+var container = document.querySelector(".container")
+var icon = document.querySelector("i")
 
-    },{
-        team : "pbsk" ,
-        primary: "pink",
-        secondary: "green",
-        captain: "srehyash ",
-        win: "0 times"
-
-    },{
-        team : "rcb" ,
-        primary: "red",
-        secondary: "black",
-        captain: "virat kohli",
-        win: "1 times"
-
-    },{
-        team : "srh" ,
-        primary: "orange",
-        secondary: "blue",
-        captain: "Pat Cummins",
-        win: "1 times"
-
-    },{
-        team : "Dc" ,
-        primary: "wheat",
-        secondary: "red",
-        captain: "Rishab panth",
-        win: "0 times"
-
-    },{
-        team : "Mi" ,
-        primary: "blue",
-        secondary: "gold",
-        captain: "rohit sharma",
-        win: "6 times"
-
-    }
-
-
-]
-// var a = Math.floor(Math.random()*arr.length);
-// console.log(a)
-
-var btn = document.querySelector("button")
-var h1 = document.querySelector("h1")
-
-
-btn.addEventListener("click", function(){
-    var num = Math.floor(Math.random()*arr.length)
-    h1.innerHTML = arr[num].team
-   document.querySelector(".box").style.backgroundColor = arr[num].primary
-   document.querySelector(".main").style.backgroundColor = arr[num].secondary
-    document.querySelector("h2").innerHTML ="(" + arr[num].captain + ") = (" + arr[num].win + " win )";
-
-
-   h1.style.color = arr[num].secondary
+container.addEventListener("dblclick",function(){
+    icon.style.transform = "translate(-50%,-50%) scale(1)";
+    icon.style.opacity = 0.8
+    setTimeout(function(){
+        icon.style.opacity = 0
+    },1000)
+    setTimeout(function(){
+    icon.style.transform = "translate(-50%,-50%) scale(0)";
+    },2000)
 })
